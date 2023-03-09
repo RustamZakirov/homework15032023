f35acdc13b404db08cb77d2af139565d3c6fca94
Rustam <rustamzakirov@mail.ru>
2023-03-09 21:50:49 +0300

    enter void

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,4 +1,13 @@
-﻿Console.Clear();
+﻿void FindString (string[] matrix)
+{
+    for (int i = 0; i < matrix.Length; i++)
+    {
+        if (matrix[i].Length < 3)
+            Console.Write($"{matrix[i]}, ");
+    }
+}
+
+Console.Clear();
 Console.WriteLine("Введите количество строк в массиве");
 int b = Convert.ToInt32(Console.ReadLine());
 string[] matrix = new string [b];
@@ -7,5 +16,5 @@ for (int i = 0; i < b; i++)
     Console.WriteLine("Введите данные массива");
     matrix[i] = Convert.ToString(Console.ReadLine());
 }
-
-Console.WriteLine($"[{string.Join(", ", matrix)}]");
+Console.Write($"[{string.Join(", ", matrix)}]");
+FindString(matrix);
