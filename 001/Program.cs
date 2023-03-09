c1445ed3ad025c8193230f5639eb48696d67e12c
Rustam <rustamzakirov@mail.ru>
2023-03-09 18:56:19 +0300

    enter string

---
+++ 001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите количество строк в массиве");
+int b = Convert.ToInt32(Console.ReadLine());
+string[] matrix = new string [b];
+for (int i = 0; i < b; i++)
+{
+    Console.WriteLine("Введите данные массива");
+    matrix[i] = Convert.ToString(Console.ReadLine());
+}
+
+Console.WriteLine($"[{string.Join(", ", matrix)}]");
